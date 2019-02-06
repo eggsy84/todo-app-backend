1e5d96ef021566642fd7e53589a262ead7d8b5e9
James Heggs <james.heggs@closconsultancy.com>
2019-02-06 11:20:47 +0000

    Delete function stub example

--- tasks.js
+++ tasks.js
@@ -4,12 +4,43 @@ const app = express();
 
 app.get('/tasks', function (request, response) {
 
-  const username = request.query.username;
+  const someTasks = [
+    {
+      id: 1,
+      description: "Buy some milk",
+      completed: false
+    },
+    {
+      id: 2,
+      description: "Buy some bread",
+      completed: false
+    },
+    {
+      id: 3,
+      description: "Buy some butter",
+      completed: false
+    }
+  ];
 
-  const someJson = {
-    message: "Hello " + username + ", how are you?"
+  response.json(someTasks);
+});
+
+app.delete('/tasks/:taskId', function (request, response) {
+
+  const taskIdToBeDeleted = request.params.taskId;
+
+  let someResponse = {
+    message: "You issued a delete request for ID: " + taskIdToBeDeleted
   };
-  response.json(someJson);
-})
+
+  if(taskIdToBeDeleted > 3 ) {
+    response.status(404);
+    someResponse = {
+      message: "Task " + taskIdToBeDeleted + " does not exist"
+    };
+  }
+
+  response.json(someResponse);
+});
 
 module.exports.handler = serverless(app);
