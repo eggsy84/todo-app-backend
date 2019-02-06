34bb7e3abff6f00fa966b480e6c5f656aab7c357
James Heggs <james.heggs@closconsultancy.com>
2019-02-06 14:21:25 +0000

    Introducing MySQL query

--- tasks.js
+++ tasks.js
@@ -1,39 +1,26 @@
-const serverless = require('serverless-http');
-const express = require('express');
+const serverless = require("serverless-http");
+const express = require("express");
 const app = express();
+const databaseService = require('./databaseservice');
 
-app.get('/tasks', function (request, response) {
+app.get("/tasks", function(request, response) {
+  databaseService.getTasks().then(function(tasks){
+    response.json(tasks);
+  })
+  .catch(function(error) {
+    response.status(500);
+    response.json(error);
+  });
+})
 
-  const someTasks = [
-    {
-      id: 1,
-      description: "Buy some milk",
-      completed: false
-    },
-    {
-      id: 2,
-      description: "Buy some bread",
-      completed: false
-    },
-    {
-      id: 3,
-      description: "Buy some butter",
-      completed: false
-    }
-  ];
-
-  response.json(someTasks);
-});
-
-app.delete('/tasks/:taskId', function (request, response) {
-
+app.delete("/tasks/:taskId", function(request, response) {
   const taskIdToBeDeleted = request.params.taskId;
 
   let someResponse = {
     message: "You issued a delete request for ID: " + taskIdToBeDeleted
   };
 
-  if(taskIdToBeDeleted > 3 ) {
+  if (taskIdToBeDeleted > 3) {
     response.status(404);
     someResponse = {
       message: "Task " + taskIdToBeDeleted + " does not exist"
